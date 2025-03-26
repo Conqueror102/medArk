ab56883d5ef1d5c3caa36e0273382f86b555bf13
Grace-babafemi <gracebabafemi993@gmail.com>
2025-03-26 12:28:45 +0100

    update

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -7,7 +7,11 @@ import PageHead from '../components/PageHead'
 import { IoIosCall } from "react-icons/io";
 import { MdOutlineMail } from "react-icons/md";
 import { IoLocationSharp } from "react-icons/io5";
+<<<<<<< HEAD
 import contactUsImage from "../assets/contact.svg";
+=======
+import contactUsImage from "../assets/contactUsPageImage.jpg";
+>>>>>>> 58c3978 (Updated job features)
 import Button from "../components/Button";
 
 const ContactUs = () => {
@@ -68,6 +72,46 @@ const ContactUs = () => {
               </div>
             </form>
           </div>
+<<<<<<< HEAD
+=======
+        </div>
+        <div className="h-[80vh] mt-[25px] w-[86%] flex justify-center items-center mb-6">
+          <div className="h-[100%] w-[60%] flex justify-start items-center">
+            <img src={contactUsImage} alt="" className="h-[90%] w-[90%]" />
+          </div>
+          <div className="h-[100%] w-[40%] flex justify-center items-center  shadow-primary/10 border-gray-200 border">
+            <form className="h-[100%] w-[90%] flex justify-around items-center flex-col">
+              <div className="h-[10%] w-[90%] flex justify-start items-center">
+                <h1 className="text-2xl font-semibold text-primary">Get In Touch!</h1>
+              </div>
+              <div className="h-[20%] w-[90%] flex justify-between items-center">
+                <div className="h-[100%] w-[47%] flex justify-around items-baseline flex-col">
+                  <label htmlFor="" className="text-[16px] text-tertiary">Your Name:</label>
+                  <input type="text" placeholder="Name:" className="w-[100%] shadow-primary/10 border-gray-200 h-[50%] border p-3 rounded-[3px] outline-0"/>
+                </div>
+                <div className="h-[100%] w-[47%] flex justify-around items-baseline flex-col">
+                  <label htmlFor="" className="text-[16px] text-tertiary">Your Email:</label>
+                  <input type="text" placeholder="Email:" className="w-[100%] shadow-primary/10 border-gray-200 h-[50%] border p-3 rounded-[3px] outline-0"/>
+                </div>
+              </div>
+              <div className="h-[20%] w-[90%] flex justify-center items-center">
+              <div className="h-[100%] w-[100%] flex justify-around items-start flex-col">
+                  <label htmlFor="" className="text-[16px] text-tertiary">Your Question:</label>
+                  <input type="text" placeholder="Subject:" className="w-[100%] shadow-primary/10 border-gray-200 h-[50%] border p-3 rounded-[3px] outline-0"/>
+                </div>
+              </div>
+              <div className="h-[25%] w-[90%] flex justify-center items-center">
+              <div className="h-[100%] w-[100%] flex justify-around items-start flex-col">
+                  <label htmlFor="" className="text-[16px] text-tertiary">Your Comment:</label>
+                  <input type="text" placeholder="Message:" className="w-[100%] shadow-primary/10 border-gray-200 h-[70%] border p-3 rounded-[3px] outline-0"/>
+                </div>
+              </div>
+              <div className="h-[15%] w-[90%] flex items-center">
+                <Button text="Send Message" className="h-[55%] w-[34%] bg-primary flex justify-center items-center text-[14px] text-white  shadow-primary/10 border-gray-200 border"/>
+              </div>
+            </form>
+          </div>
+>>>>>>> 58c3978 (Updated job features)
         </div>
       </div>
     
